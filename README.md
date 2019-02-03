058144720a5e38af00e29a2473878556311eab72
Pawel Kondraciuk <pawel.kondraciuk@instapage.com>
2019-02-03 15:40:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Spontaniczna platforma turniejowa
+
+A simple tournament management system
+
+## Useful commands
+
+Install dependencies
+
+    npm install
+
+Start development server with hot reloading (open `http://localhost:8080` and find your way through the directory listing)
+
+    npm start
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
